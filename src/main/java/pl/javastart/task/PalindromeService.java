6c2f34e342669d66592fe6ed8e47d0f7b3dba74e
Mateusz Rojek <mateuszr2047@gmail.com>
2023-10-23 21:37:25 +0200

    Commit1

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -1,9 +1,19 @@
 package pl.javastart.task;
 
-public class PalindromeService {
+import java.util.Arrays;
 
-    // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
+public class PalindromeService {
     public boolean isPalindrome(int[] array) {
+
+        if (array != null && array.length != 0) {
+            for (int i = 0; i < array.length / 2; i++) {
+                if (array[i] != array[array.length - i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         return false;
     }
 }
